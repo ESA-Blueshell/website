b9b9b7c5b2429998ffe4bf336917743b586f5849
Jakub Stepniewski <jakstepniew@gmail.com>
2025-02-27 18:08:53 +0100

    Add container debugging capabilities

--- APIGateway/src/main/java/io/github/jakstepn/apigateway/MainController.java
+++ APIGateway/src/main/java/io/github/jakstepn/apigateway/MainController.java
@@ -1,8 +1,16 @@
 package io.github.jakstepn.apigateway;
 
 import io.github.jakstepn.common.TestClass;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 
 @RestController
 public class MainController {
@@ -10,5 +18,39 @@ public class MainController {
     @RequestMapping("/")
     public String home() {
         return TestClass.Test + "test";
+    }
+
+    @RequestMapping("/blog")
+    public ResponseEntity<String> blog() {
+        try {
+            // URL of the localhost endpoint
+            URI uri = new URI("http://localhost:80/");
+            URL url = uri.toURL();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            // Set the request method to GET
+            connection.setRequestMethod("GET");
+
+            // Get the response code
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response Code: " + responseCode);
+
+            // Read the response
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // Print the response
+            return new ResponseEntity<>(response.toString(), HttpStatus.OK);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Could not send request!", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- README.md
+++ README.md
@@ -10,3 +10,12 @@ It is recommended to prepare a local Maven repository
 under `~/.m2` directory. In Jetbrains IntelliJ IDEA you can do it
 from the Maven panel. Local repository is copied to the main directory of the
 project, where it is used by the Dockerfile to build the image quicker.
+
+### How to connect debugger to a running container
+
+To debug a specific module like (_APIGateway_) open IntelliJ:
+1. Run/Debug Configurations
+2. Add new configuration (+ on the top left)
+3. Remote JVM Debug
+4. Set the port to 5005 (For all containers 5005 is exposed by default [debug docker-compose is coming in the next commit])
+5. Change Classpath to the module you would like to debug

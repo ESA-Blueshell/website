da44957e49ad6ddcbe93987e8be69f29a6b8b57f
Fabian Simon <77444050+fabiansimon@users.noreply.github.com>
2025-03-28 12:20:37 +0100

    inital mockup of service; tracking logic; shareable generation

--- Telemetry/src/main/java/net/blueshell/telemetry/MainController.java
+++ Telemetry/src/main/java/net/blueshell/telemetry/MainController.java
@@ -1,12 +1,60 @@
 package net.blueshell.telemetry;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import net.blueshell.common.Blog;
+import net.blueshell.common.Event;
+import net.blueshell.telemetry.model.MetricResponse;
+import net.blueshell.telemetry.service.TelemetryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class MainController {
-    @GetMapping("/")
-    public String SayHello() {
-        return "Telemetry";
+    @Autowired
+    private TelemetryService telemetryService;
+
+    @GetMapping("/health")
+    public Boolean checkHealth() {
+        return true;
     }
+
+    @GetMapping("/metric")
+    public ResponseEntity<MetricResponse> getOverallMetric(
+            @RequestParam("timespan") int days,
+            @RequestParam("type") String type
+    ) {
+        MetricResponse res = telemetryService.getTotalMetrics(days, type);
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/metric/{id}")
+    public ResponseEntity<MetricResponse> getMetricForId(
+            @PathVariable("id") String id,
+            @RequestParam("timespan") int days
+    ) {
+        MetricResponse res = telemetryService.getMetricsForId(days, id);
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/shareable/{id}")
+    public ResponseEntity<String> trackVisit(
+            @PathVariable("id") String id,
+            @RequestParam("src") String src
+    ) {
+        String redirectUrl = telemetryService.trackAndExchangeURL(id, src);
+        return ResponseEntity.status(302).header("Location", redirectUrl).build();
+    }
+
+    @PostMapping("/create-shareable")
+    public ResponseEntity<String> createShareable(
+            @RequestParam("url") String url,
+            @RequestParam("type") String type
+    ) {
+        String shareableId = telemetryService.createShareable(url, type);
+        return ResponseEntity.ok(shareableId);
+    }
+
 }
+

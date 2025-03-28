e77fc0ee57fa5800c9f49f76dd37c75d4cdb1b92
Fabian Simon <77444050+fabiansimon@users.noreply.github.com>
2025-03-28 10:27:29 +0100

    Rebase via merge. 'social-media-service' rebased on 'main'.

--- Common/src/main/java/net/blueshell/common/Event.java
+++ Common/src/main/java/net/blueshell/common/Event.java
@@ -1,18 +1,17 @@
 package net.blueshell.common;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-@Data
 public class Event {
-    private String id;
     private String title;
-    private String content;
+    private String description;
+    private String location;
+    private String url;
 }
-

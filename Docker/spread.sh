21f0cc6a026d1a618b679e600990b5dc22311313
Joris Jonkers <joriswouter@live.nl>
2025-04-07 18:53:39 +0200

    Update of docker files to allow for hot-reloading via docker-compose.dev.yml

--- Docker/spread.sh
+++ Docker/spread.sh
@@ -7,8 +7,9 @@ COMMON_DIR="$(cd "$(dirname "core-commons")" && pwd)"
 # Get the parent directory
 PARENT_DIR="$(dirname "$SCRIPT_DIR")"
 
-# Read the content of the Dockerfile in the script's directory
-DOCKERFILE_TEMPLATE=$(<"$SCRIPT_DIR/dockerfile")
+# Read the content of the Dockerfile templates
+DOCKERFILE_BUILD_TEMPLATE=$(<"$SCRIPT_DIR/dockerfile-build")
+DOCKERFILE_DEV_TEMPLATE=$(<"$SCRIPT_DIR/dockerfile-dev")
 KUBERNETES_DEPLOY_TEMPLATE=$(<"$SCRIPT_DIR/deployment")
 KUBERNETES_SERVICE_TEMPLATE=$(<"$SCRIPT_DIR/service")
 KUBERNETES_GATEWAY_SERVICE_TEMPLATE=$(<"$SCRIPT_DIR/gateway-service")
@@ -28,44 +29,58 @@ done
 
 # Iterate over each sibling directory
 for DIR in "$PARENT_DIR"/*/; do
-    # Skip if not a directory or if it is the script's directory
+    # Skip if not a directory or if it is excluded
     [ -d "$DIR" ] || continue
-    [ "$DIR" == "$SCRIPT_DIR/" ] && continue
-    [ "$(basename "$DIR")" == "core-commons" ] && continue
-    [ "$(basename "$DIR")" == "db-commons" ] && continue
-    [ "$(basename "$DIR")" == "Docker" ] && continue
-    [ "$(basename "$DIR")" == "Scripts" ] && continue
-    [ "$(basename "$DIR")" == "files" ] && continue
-    [ "$(basename "$DIR")" == "telemetry-client" ] && continue
-    [ "$(basename "$DIR")" == "frontend" ] && continue
+    [[ "$DIR" == "$SCRIPT_DIR/" || \
+       "$(basename "$DIR")" == "core-commons" || \
+       "$(basename "$DIR")" == "db-commons" || \
+       "$(basename "$DIR")" == "Docker" || \
+       "$(basename "$DIR")" == "Scripts" || \
+       "$(basename "$DIR")" == "files" || \
+       "$(basename "$DIR")" == "telemetry-client" || \
+       "$(basename "$DIR")" == "frontend" ]] && continue
 
-    # Get the name of the directory
     SERVICE_NAME=$(basename "$DIR")
     SERVICE_NAME_LOWER=$(basename "$SERVICE_NAME" | tr '[:upper:]' '[:lower:]')
 
-    # Check if Dockerfile already exists
-    if [ -f "${DIR}Dockerfile" ] && [ "$FORCE" = false ]; then
-        echo "Dockerfile already exists in ${DIR}. Use -f to overwrite."
-        continue
+    # Generate Dockerfile-build
+    DOCKERFILE_BUILD_CONTENT="${DOCKERFILE_BUILD_TEMPLATE//\$\{SERVICE_NAME\}/$SERVICE_NAME}"
+    DOCKERFILE_BUILD_PATH="${DIR}Dockerfile-build"
+    if [ -f "$DOCKERFILE_BUILD_PATH" ] && [ "$FORCE" = false ]; then
+        echo "Dockerfile-build already exists in ${DIR}. Use -f to overwrite."
+    else
+        echo "$DOCKERFILE_BUILD_CONTENT" > "$DOCKERFILE_BUILD_PATH"
+        echo "Created $DOCKERFILE_BUILD_PATH"
     fi
 
-    # Replace ${SERVICE_NAME} with the actual directory name
-    DOCKERFILE_CONTENT="${DOCKERFILE_TEMPLATE//\$\{SERVICE_NAME\}/$SERVICE_NAME}"
+    # Generate dockerfile-dev
+    DOCKERFILE_DEV_CONTENT="${DOCKERFILE_DEV_TEMPLATE//\$\{SERVICE_NAME\}/$SERVICE_NAME}"
+    DOCKERFILE_DEV_PATH="${DIR}Dockerfile-dev"
+    if [ -f "$DOCKERFILE_DEV_PATH" ] && [ "$FORCE" = false ]; then
+        echo "Dockerfile-dev already exists in ${DIR}. Use -f to overwrite."
+    else
+        echo "$DOCKERFILE_DEV_CONTENT" > "$DOCKERFILE_DEV_PATH"
+        echo "Created $DOCKERFILE_DEV_PATH"
+    fi
+
+    # Generate Kubernetes templates if applicable
     KUBERNETES_DEPLOY_CONTENT="${KUBERNETES_DEPLOY_TEMPLATE//\$\{SERVICE_NAME\}/$SERVICE_NAME_LOWER}"
     KUBERNETES_SERVICE_CONTENT="${KUBERNETES_SERVICE_TEMPLATE//\$\{SERVICE_NAME\}/$SERVICE_NAME_LOWER}"
 
-    # Create the Dockerfile in the directory
-    echo "$DOCKERFILE_CONTENT" > "${DIR}Dockerfile"
-    echo "Created ${DIR}Dockerfile"
-
-    # Perform an action if the directory name is APIGateway
     if [ "$SERVICE_NAME" == "APIGateway" ]; then
-        KUBERNETES_SERVICE_CONTENT="${KUBERNETES_GATEWAY_SERVICE_TEMPLATE//\$\{SERVICE_NAME\}/$SERVICE_NAME_LOWER}"
         KUBERNETES_DEPLOY_CONTENT="${KUBERNETES_GATEWAY_DEPLOY_TEMPLATE//\$\{SERVICE_NAME\}/$SERVICE_NAME_LOWER}"
+        KUBERNETES_SERVICE_CONTENT="${KUBERNETES_GATEWAY_SERVICE_TEMPLATE//\$\{SERVICE_NAME\}/$SERVICE_NAME_LOWER}"
     fi
 
-    echo "$KUBERNETES_DEPLOY_CONTENT" > "${DIR}/${SERVICE_NAME_LOWER}-deployment.yaml"
-    echo "Created ${DIR_LOWER}${SERVICE_NAME_LOWER}-deployment.yaml"
-    echo "$KUBERNETES_SERVICE_CONTENT" > "${DIR}/${SERVICE_NAME_LOWER}-service.yaml"
-    echo "Created ${DIR_LOWER}${SERVICE_NAME_LOWER}-service.yaml"
+    KUBERNETES_DEPLOY_FILE="${DIR}${SERVICE_NAME_LOWER}-deployment.yaml"
+    KUBERNETES_SERVICE_FILE="${DIR}${SERVICE_NAME_LOWER}-service.yaml"
+
+    if [ "$FORCE" = true ] || ! { [ -f "$KUBERNETES_DEPLOY_FILE" ] && [ -f "$KUBERNETES_SERVICE_FILE" ]; }; then
+        echo "$KUBERNETES_DEPLOY_CONTENT" > "$KUBERNETES_DEPLOY_FILE"
+        echo "Created $KUBERNETES_DEPLOY_FILE"
+        echo "$KUBERNETES_SERVICE_CONTENT" > "$KUBERNETES_SERVICE_FILE"
+        echo "Created $KUBERNETES_SERVICE_FILE"
+    else
+        echo "Kubernetes templates already exist in ${DIR}. Use -f to overwrite."
+    fi
 done

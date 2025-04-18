f769a8d5fc8851731c027e40140ed7dcf52fb403
Jakub Stepniewski <jakstepniew@gmail.com>
2025-04-18 23:43:49 +0200

    Update Kubernetes deployment scripts and .gitignore; standardize file paths and add script deployment files

--- Docker/spread.sh
+++ Docker/spread.sh
@@ -35,7 +35,7 @@ for DIR in "$PARENT_DIR"/*/; do
        "$(basename "$DIR")" == "core-commons" || \
        "$(basename "$DIR")" == "db-commons" || \
        "$(basename "$DIR")" == "Docker" || \
-       "$(basename "$DIR")" == "Scripts" || \
+       "$(basename "$DIR")" == "script" || \
        "$(basename "$DIR")" == "files" || \
        "$(basename "$DIR")" == "telemetry-client" || \
        "$(basename "$DIR")" == "frontend" || \
--- script/launch_kubernetes.sh
+++ script/launch_kubernetes.sh
@@ -1,13 +1,25 @@
-kubectl apply -f ../APIGateway/apigateway-deployment.yaml
-kubectl apply -f ../BlogService/blogservice-deployment.yaml
-kubectl apply -f ../EmailParser/emailparser-deployment.yaml
-kubectl apply -f ../EventParser/eventparser-deployment.yaml
-kubectl apply -f ../SocialMediaService/socialmediaservice-deployment.yaml
-kubectl apply -f ../Telemetry/telemetry-deployment.yaml
+kubectl apply -f ../api-gateway/api-gateway-deployment.yaml
+kubectl apply -f ../blog-parser/blog-parser-deployment.yaml
+kubectl apply -f ../blog-service/blog-service-deployment.yaml
+kubectl apply -f ../email-parser/email-praser-deployment.yaml
+kubectl apply -f ../eureka-server/eureka-server-deployment.yaml
+kubectl apply -f ../event-parser/event-parser-deployment.yaml
+kubectl apply -f ../file-service/file-service-deployment.yaml
+kubectl apply -f ../social-media-service/social-media-service-deployment.yaml
+kubectl apply -f ../telemetry-service/telemetry-service-deployment.yaml
 
-kubectl apply -f ../APIGateway/apigateway-service.yaml
-kubectl apply -f ../BlogService/blogservice-service.yaml
-kubectl apply -f ../EmailParser/emailparser-service.yaml
-kubectl apply -f ../EventParser/eventparser-service.yaml
-kubectl apply -f ../SocialMediaService/socialmediaservice-service.yaml
-kubectl apply -f ../Telemetry/telemetry-service.yaml
+kubectl apply -f ../script/mariadb-deployment
+kubectl apply -f ../script/rabbitmq-deployment
+
+kubectl apply -f ../api-gateway/api-gateway-service.yaml
+kubectl apply -f ../blog-parser/blog-parser-service.yaml
+kubectl apply -f ../blog-service/blog-service-service.yaml
+kubectl apply -f ../email-parser/email-praser-service.yaml
+kubectl apply -f ../eureka-server/eureka-server-service.yaml
+kubectl apply -f ../event-parser/event-parser-service.yaml
+kubectl apply -f ../file-service/file-service-service.yaml
+kubectl apply -f ../social-media-service/social-media-service-service.yaml
+kubectl apply -f ../telemetry-service/telemetry-service-service.yaml
+
+kubectl apply -f ../script/mariadb-service.yaml
+kubectl apply -f ../script/rabbitmq-service.yaml

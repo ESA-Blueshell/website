056798d054433ad79ab8d5c9fc52c1c805fa2a19
Joris Jonkers <joriswouter@live.nl>
2025-04-18 18:03:16 +0200

    Repaired docker compose tests.

--- blog-service/src/main/java/net/blueshell/blogservice/controller/BlogController.java
+++ blog-service/src/main/java/net/blueshell/blogservice/controller/BlogController.java
@@ -34,13 +34,13 @@ public class BlogController extends IdentityProvider {
         blogService.create(blog);
     }
 
-    @GetMapping("/blogs")
+    @GetMapping
     public List<InternalBlogDTO> findAll() {
         log.info("Principal: {}", getPrincipal());
         return blogMapper.toDTOs(blogService.findAll());
     }
 
-    @GetMapping("/blogs/{id}")
+    @GetMapping("/{id}")
     public InternalBlogDTO findById(@PathVariable UUID id) {
         return blogMapper.toDTO(blogService.findById(id));
     }

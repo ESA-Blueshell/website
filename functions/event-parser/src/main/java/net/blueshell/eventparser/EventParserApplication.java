9cec9e8b67fac7720f03932774e62ad68c381cc6
Joris Wouter Jonkers <joriswouter@live.nl>
2025-06-28 20:52:46 +0200

    Becakup before moving to Kubernetes service discovery

--- functions/event-parser/src/main/java/net/blueshell/eventparser/EventParserApplication.java
+++ functions/event-parser/src/main/java/net/blueshell/eventparser/EventParserApplication.java
@@ -1,7 +1,5 @@
 package net.blueshell.eventparser;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;

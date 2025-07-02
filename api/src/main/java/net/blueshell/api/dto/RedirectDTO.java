d357a577e65ec2bed0504c4f5430e10c5b191010
Joris Wouter Jonkers <joriswouter@live.nl>
2025-07-02 14:05:19 +0200

    Repairing poms and general structure of the project

--- api/src/main/java/net/blueshell/api/dto/RedirectDTO.java
+++ api/src/main/java/net/blueshell/api/dto/RedirectDTO.java
@@ -10,7 +10,7 @@ import java.sql.Timestamp;
 @EqualsAndHashCode(callSuper = true)
 public class RedirectDTO extends BaseDTO {
     private String id;
-    private Timestamp created;
+    private Timestamp createdAt;
     private TelemetryDTO telemetry;
 }
 
--- run.sh
+++ run.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-# Check for the -f flag
-FORCE=false
-while getopts "f" opt; do
-  case $opt in
-    f)
-      FORCE=true
-      ;;
-    *)
-      ;;
-  esac
-done
-
-if [ "$FORCE" = true ]; then
-  ./spread.sh -f
-else
-  ./spread.sh
-fi
-
-docker compose -f docker/docker-compose.build.yml up --build -d
+docker compose -f docker-compose.build.yml up --build -d
 
 echo "Docker containers started"

e6666c0fb76ee1e054679dfd89c3e8040865d591
j.w.jonkers <joris.jonkers@nedap.com>
2025-09-05 12:34:15 +0200

    Further update address model

--- api/src/main/java/net/blueshell/api/dto/AddressDTO.java
+++ api/src/main/java/net/blueshell/api/dto/AddressDTO.java
@@ -5,11 +5,10 @@ import lombok.Data;
 @Data
 public class AddressDTO {
     private Long id;
-    private String address;
-    private String houseNumber;
-    private String postalCode;
+    private String country;
     private String city;
     private String street;
-    private String country;
+    private String houseNumber;
+    private String zipCode;
     private String createdAt;
 }

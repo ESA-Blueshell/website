ac1f8bf64f4c4ca5387448c54f6d6895ddd685bb
j.w.jonkers <joris.jonkers@nedap.com>
2025-09-18 10:15:34 +0200

    Remove bot.

--- bot/src/container/types.ts
+++
@@ -1,6 +0,0 @@
-export const TYPES = {
-    CacheManager: Symbol.for('CacheManager'),
-    DiscordService: Symbol.for('DiscordService'),
-    DiscordClient: Symbol.for('DiscordClient'),
-    ApiController: Symbol.for('ApiController')
-};

07845f9d8d5e4c15a4c25a012d792e9c85529c98
j.w.jonkers <joris.jonkers@nedap.com>
2025-10-28 16:14:52 +0100

    Remove unnecessary sockets and configurations. Comment out redirects/telemetries as these are not currently used.

--- api/src/main/java/net/blueshell/api/service/RedirectService.java
+++ api/src/main/java/net/blueshell/api/service/RedirectService.java
@@ -1,47 +1,46 @@
-package net.blueshell.api.service;
-
-import lombok.extern.slf4j.Slf4j;
-import net.blueshell.api.base.BaseModelService;
-import net.blueshell.api.model.Redirect;
-import net.blueshell.api.model.Telemetry;
-import net.blueshell.api.repository.RedirectRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.OffsetDateTime;
-import java.util.List;
-
-@Service
-@Slf4j
-public class RedirectService extends BaseModelService<Redirect, RedirectRepository> {
-    private final TelemetryService telemetryService;
-    private final WebSocketMessageService webSocketMessageService;
-
-    @Autowired
-    public RedirectService(RedirectRepository repository, TelemetryService telemetryService,
-                           WebSocketMessageService webSocketMessageService) {
-        super(repository);
-        this.telemetryService = telemetryService;
-        this.webSocketMessageService = webSocketMessageService;
-    }
-
-    @Transactional
-    public String createRedirect(Long telemetryId) {
-        log.info("TL Creating redirect for telemetry");
-        Telemetry telemetry = telemetryService.findById(telemetryId);
-        Redirect newRedirect = new Redirect(telemetry);
-        log.info("TL Created redirect for telemetry: {}", telemetry.getId());
-        create(newRedirect);
-
-        List<Redirect> redirects = findAll();
-        log.info("TL Found all redirects");
-        webSocketMessageService.sendMessage("/clicks/updates", redirects);
-
-        return "redirect:%s".formatted(telemetry.getUrl());
-    }
-
-    public List<Redirect> findCreatedAtBetween(OffsetDateTime from, OffsetDateTime to) {
-        return repository.findCreatedAtBetween(from, to);
-    }
-}
+//package net.blueshell.api.service;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import net.blueshell.api.base.BaseModelService;
+//import net.blueshell.api.model.Redirect;
+//import net.blueshell.api.model.Telemetry;
+//import net.blueshell.api.repository.RedirectRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.time.OffsetDateTime;
+//import java.util.List;
+//
+//@Service
+//@Slf4j
+//public class RedirectService extends BaseModelService<Redirect, RedirectRepository> {
+//    private final TelemetryService telemetryService;
+//
+//    @Autowired
+//    public RedirectService(RedirectRepository repository, TelemetryService telemetryService,
+//                           WebSocketMessageService webSocketMessageService) {
+//        super(repository);
+//        this.telemetryService = telemetryService;
+//        this.webSocketMessageService = webSocketMessageService;
+//    }
+//
+//    @Transactional
+//    public String createRedirect(Long telemetryId) {
+//        log.info("TL Creating redirect for telemetry");
+//        Telemetry telemetry = telemetryService.findById(telemetryId);
+//        Redirect newRedirect = new Redirect(telemetry);
+//        log.info("TL Created redirect for telemetry: {}", telemetry.getId());
+//        create(newRedirect);
+//
+//        List<Redirect> redirects = findAll();
+//        log.info("TL Found all redirects");
+//        webSocketMessageService.sendMessage("/clicks/updates", redirects);
+//
+//        return "redirect:%s".formatted(telemetry.getUrl());
+//    }
+//
+//    public List<Redirect> findCreatedAtBetween(OffsetDateTime from, OffsetDateTime to) {
+//        return repository.findCreatedAtBetween(from, to);
+//    }
+//}

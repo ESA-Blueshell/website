ffd728b109e2dcdbae6bbd4756b002db196f2b99
j.w.jonkers <joris.jonkers@nedap.com>
2026-02-02 11:46:07 +0100

    Add lombok to test configuration

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
 
     compileOnly("org.projectlombok:lombok:1.18.42")
     annotationProcessor("org.projectlombok:lombok:1.18.42")
+    testCompileOnly("org.projectlombok:lombok:1.18.42")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.42")
     compileOnly("org.projectlombok:lombok-mapstruct-binding:0.2.0")
     annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 

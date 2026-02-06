b55009e201766ad6c4f66abd9d75aa5fd01bcfab
j.w.jonkers <joris.jonkers@nedap.com>
2026-02-06 12:18:41 +0100

    Repair DtoFactoryTestSupport.kt

--- api/src/test/kotlin/net/blueshell/api/factory/dto/DtoFactoryTestSupport.kt
+++ api/src/test/kotlin/net/blueshell/api/factory/dto/DtoFactoryTestSupport.kt
@@ -26,7 +26,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
+import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
@@ -147,7 +147,7 @@ abstract class DtoFactoryTestSupport {
     }
 }
 
-@Configuration
+@TestConfiguration
 @ComponentScan(basePackages = ["net.blueshell.api.factory.dto"])
 @Import(FactoryConfig::class, PhoneNumberConfig::class)
 class DtoFactoryTestConfig {

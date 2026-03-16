af372001e8328559e0c2713456f2ce48e9d75c5a
j.w.jonkers <joris.jonkers@nedap.com>
2026-03-16 14:54:05 +0100

    Repair frontend tests

--- frontend/tests/setup.ts
+++ frontend/tests/setup.ts
@@ -1,5 +1,11 @@
 import {afterEach, beforeEach, vi} from "vitest"
 import {config} from "@vue/test-utils"
+import {createVuetify} from "vuetify"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
+
+const vuetify = createVuetify({components, directives})
+config.global.plugins = [vuetify]
 
 config.global.stubs = {
   RouterLink: {

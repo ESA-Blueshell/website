8c1f0a507ed9aa64086f89af4f5dee4490b0356f
j.w.jonkers <joris.jonkers@nedap.com>
2026-04-21 19:36:38 +0200

    fix: use platform() BOM in libs/kotlin-common instead of dependency-management plugin

--- libs/kotlin-common/build.gradle.kts
+++ libs/kotlin-common/build.gradle.kts
@@ -1,19 +1,13 @@
 plugins {
     id("kotlin-conventions")
-    id("io.spring.dependency-management")
 }
 
 group = "net.blueshell"
 version = "0.0.1-SNAPSHOT"
 
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.boot:spring-boot-dependencies:4.0.3")
-    }
-}
-
 // Shared Kotlin helpers for services (Vault/OIDC/JWT/forward-auth).
 dependencies {
+    compileOnly(platform("org.springframework.boot:spring-boot-dependencies:4.0.3"))
     compileOnly("org.springframework.security:spring-security-oauth2-jose")
     compileOnly("org.springframework.boot:spring-boot-starter-web")
 }
